d09cd83db5e7a57f750777dedbcbe6d2638dd32c
vievk <vivektharhi@gmail.com>
2025-09-15 21:09:55 +0530

    create user using mysql database

--- spring/SpringCore/src/main/java/org/interview/dto/UserDto.java
+++ spring/SpringCore/src/main/java/org/interview/dto/UserDto.java
@@ -6,5 +6,9 @@ import lombok.Data;
 @Data
 public class UserDto {
 
- private String message;
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String role;
 }
